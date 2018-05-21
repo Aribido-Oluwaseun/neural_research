002d779aa7488b4a66c5003ed2b4439416acff2f
Aribido-Oluwaseun <oja@gatech.edu>
2018-05-21 16:40:08 -0400

    restructured code

--- stam.py
+++ stam.py
@@ -64,80 +64,95 @@ class STAM:
     def balanced_order(self, data, lbl):
         print 'please wait, generating balanced order data...'
         build_indices = list()
-        df = pd.DataFrame(data=data, columns=range(data.shape[1]))
-        df['lbl'] = lbl
-        df = df.sort_values('lbl') # sort based on the labels
-        ten_value_index = [list(df['lbl']).index(x) for x in range(10)]
+        data['lbl'] = lbl
+        data = data.sort_values('lbl') # sort based on the labels
+        ten_value_index = [list(data['lbl']).index(x) for x in range(10)]
         ten_value_index = np.asarray(ten_value_index)
-        indices = np.asarray(df.index.tolist())
-        build_indices.extend(ten_value_index)
+        indices = np.asarray(data.index.tolist())
         print 'building indices...'
-        for i in range(int(data.shape[0]/10.0)-10):
+        for i in range(int(data.shape[0]/10.0)):
             ten_value_index += 1
-            temp = np.random.choice(a=indices[ten_value_index], size=len(ten_value_index))
+            temp = np.random.choice(a=indices[ten_value_index], size=len(ten_value_index), replace=False)
             build_indices.extend(temp)
         print 'balanced order indices generated successfully!'
-        lbl = df['lbl']
-        df = df.iloc[build_indices, :-1]
+
+        data = data.iloc[build_indices, :]
         print 'balanced order data created successfully...\n'
-        return df, lbl
+        return data
 
     def random_order(self, data, lbl):
         print 'please wait, generating random order data...'
         df = pd.DataFrame(data=data, columns=range(data.shape[1]))
         df['lbl'] = lbl
         df = df.sample(frac=1)
-        lbl = df['lbl']
-        return df.iloc[:, :-1], lbl
+        return df
+
 
     def neural_algo(self, data,
                     lbl,
                     W=None,
                     num_clusters=10,
                     iterative_prediction=False,
-                    mapping_matrix=False,
-                    order='BALANCED_order',
+                    get_mapping_matrix=False,
+                    order='BALANCED_ORDER',
                     W_init = 'RND_INT'):
 
-        matrix = pd.DataFrame(data=np.zeros([num_clusters, 10]), index=self.cluster_labels[0:num_clusters])
+        matrix = pd.DataFrame(data=np.zeros([num_clusters, 10]), index=self.cluster_labels[0:num_clusters], dtype=int)
         # we hard code 10 to be the number of columns because there are 10
         # classes in the mnist dataset no matter what.
         # Initialize W, thetha and n
         indices_of_W = range(0, num_clusters)
         track_predictions = list()
+        df = None
         if order == 'BALANCED_ORDER':
-            data, lbl = self.balanced_order(data=data, lbl=lbl)
-        if order == 'RANDOM_ORDER':
-            data, lbl = self.random_order(data=data, lbl=lbl)
+            df = self.balanced_order(data=data, lbl=lbl)
+
+        elif order == 'RANDOM_ORDER':
+            df = self.random_order(data=data, lbl=lbl)
+
+        else:
+            df = data
+            df['lbl'] = lbl
 
         print 'proceeding with neural-algorithm...'
         if W is None:
             # We give this option in case we have some learned W for which we want to optimize with further training
             # This is useful if we want to train our algorithm in several epochs.
-            if W_int == 'RND_INT':
-                W = pd.DataFrame(np.random.rand(num_clusters, data.shape[1]))
-            elif W_int == 'OPT_INT':
-                W = pd.DataFrame(np.zeros([num_clusters, data.shape[1]]))
-                digits = range(10)
-                for i in digits:
-                    W.iloc[i, :] = data.iloc[lbl.index(i), :]
+            if W_init == 'RND_INT':
+                print 'initializing W with random digits...'
+                W = pd.DataFrame(np.random.rand(num_clusters, df.shape[1]-1))
+            elif W_init == 'OPT_INT':
+                # Here, we initialize W with each digit class.
+                print 'initializing W with each digit class'
+                W = pd.DataFrame(np.zeros([num_clusters, df.shape[1]-1]))
+                digits = list(np.random.choice(a=range(10), size=num_clusters, replace=False))
+                while len(digits) > 0:
+                    # We search over the labels for each digit and when we find it, we initialize W with it
+                    j = 0
+                    while df.iloc[df.index.values[j], -1] != digits[0]:
+                        j += 1
+                    else:
+                        W.iloc[len(digits)-1, :] = df.iloc[df.index.values[j], :-1]
+                        digits.pop(0)
             else:
                 raise(STAM_Exception('please specify W_int=RND_INT or W_int=OPT_INT'))
-        print 'W initialized...'
+        print 'W initialized...\n'
 
         thetha = np.zeros([W.shape[0], ])
         n_i = np.ones([W.shape[0], ])
-        # We iterate over T times
+        # We iterate over the examples T times. Where T is the length of each example.
         print 'iterating over examples...'
-        for T in range(0, data.shape[0]):
-            x_T = data.iloc[T, :]
+
+        for T in df.index.tolist():
+
+            x_T = df.iloc[T, :-1]
             c, y_i_t, z_T = self.a_T(W, x_T, thetha)
             n_i[c] = n_i[c] + 1
             W.iloc[c, :] = W.iloc[c, :] + 1 / n_i[c] * (2 * x_T - W.iloc[c, :])
             thetha[c] = thetha[c] + 1 / n_i[c] * (z_T - thetha[c])
 
-            if mapping_matrix:
-                matrix.iloc[c, self.train_lbl[T]] += 1
+            if get_mapping_matrix:
+                matrix.iloc[c, int(df.iloc[T, -1])] += 1
 
             if iterative_prediction:
                 print 'iteractive prediction activated. Generating accuracy for each example...'
@@ -150,10 +165,10 @@ class STAM:
 
         print 'neural algorithm completed! \n'
         if iterative_prediction:
-            return 0.5*W, track_predictions
-        if mapping_matrix:
+            return 0.5 * W, track_predictions
+        if get_mapping_matrix:
             return 0.5 * W, matrix
-        return 0.5 * W
+        return 0.5 * W, 0
 
     def a_T(self, W_T, X_T, thetha_T):
         a_i_T = -np.dot(W_T, X_T) + thetha_T
@@ -163,22 +178,22 @@ class STAM:
         z_T = -1*a_i_T[c]
         return c, y_i_t, z_T
 
-    def run(self, data, lbl, W=None, num_clusters=10, display=None, order=None, get_iterative_prediction=False,
-            get_mapping_matrix=False):
+    def run(self, data, lbl, W=None, num_clusters=10, order=None, get_iterative_prediction=False,
+            get_mapping_matrix=False, W_init=None):
 
         if get_iterative_prediction:
-            W, pred = self.neural_algo(data, lbl, W=None, num_clusters=10, iterative_prediction=False,
-                                       mapping_matrix=False, order='BALANCED_order', W_init='RND_INT')
+            W, pred = self.neural_algo(data, lbl, W=W, num_clusters=num_clusters, iterative_prediction=get_iterative_prediction,
+                                       get_mapping_matrix=get_mapping_matrix, order=order, W_init=W_init)
             W.to_pickle(SAVE_MODEL_PATH)
             return W, pred
         elif get_mapping_matrix:
-            W, matrix = self.neural_algo(data, lbl, W=None, num_clusters=10, iterative_prediction=False,
-                                       mapping_matrix=False, order='BALANCED_order', W_init='RND_INT')
+            W, matrix = self.neural_algo(data, lbl, W=W, num_clusters=num_clusters, iterative_prediction=get_iterative_prediction,
+                                       get_mapping_matrix=get_mapping_matrix, order=order, W_init=W_init)
             W.to_pickle(SAVE_MODEL_PATH)
             return W, matrix
         else:
             W.to_pickle(SAVE_MODEL_PATH)
-            return W
+            return W, 0
 
     def learn_labels(self, validation_data, validation_lbl, W):
         """This function attempts to assign labels to the various images in the converged W"""
@@ -262,18 +277,20 @@ class Experiments:
         plt.xlabel('number of training examples')
         plt.show()
 
-    def learn_w_with_set_clusters(self, num_clusters, shuffle=False, use_saved_W=False, num_epochs=0, mapping_matrix=False, balanced_order=False):
+    def learn_w_with_set_clusters(self, data, lbl, num_clusters, shuffle=False, use_saved_W=False, num_epochs=0, get_mapping_matrix=False, order=None, W_init=None):
         matrix = None
         for i in range(num_epochs):
             if use_saved_W:
                 if os.path.exists(SAVE_MODEL_PATH):
                     with open(SAVE_MODEL_PATH, 'rb') as f:
                         W = pickle.load(f)
-                        matrix = self.stam_obj.run(num_clusters, display=False, shuffle=shuffle, W=W, mapping_matrix=mapping_matrix, balanced_order=balanced_order)
+                        _, matrix = self.stam_obj.run(data, lbl, W=W, num_clusters=num_clusters,
+                                                      get_mapping_matrix=get_mapping_matrix, order=order, W_init=W_init)
                 else:
-                    matrix = self.stam_obj.run(num_clusters, display=False, shuffle=shuffle, mapping_matrix=mapping_matrix, balanced_order=balanced_order)
+                    raise(pickle.PickleError('An error occurred with the file path!'))
             else:
-                matrix = self.stam_obj.run(num_clusters, display=False, shuffle=shuffle, mapping_matrix=mapping_matrix, balanced_order=balanced_order)
+                _, matrix = self.stam_obj.run(data, lbl, num_clusters=num_clusters,
+                                              get_mapping_matrix=get_mapping_matrix, order=order, W_init=W_init)
         return matrix
 
     def moving_average(self, values, window):
@@ -282,6 +299,9 @@ class Experiments:
         return sma
 
     def run_experiments(self):
+        train, train_lbl = self.stam_obj.get_prep_data(type='train')
+        test, test_lbl = self.stam_obj.get_prep_data(type='test')
+        num_clusters = 10
         # settings for DataFrame display
         pd.set_option('display.max_rows', 1000)
         pd.set_option('display.max_columns', 1000)
@@ -289,13 +309,13 @@ class Experiments:
         #self.learn_labels_after_training(train=False)
         #self.check_training_accuracy(train=False)
         #self.track_errors_during_training(num_of_clusters=15)
-        matrix = self.learn_w_with_set_clusters(num_clusters=10, shuffle=False, use_saved_W=True, num_epochs=3,
-                                                 mapping_matrix=True, balanced_order=True)
-        writer = pd.ExcelWriter('balanced_order.xlsx')
+        matrix = self.learn_w_with_set_clusters(train, train_lbl, num_clusters=num_clusters, use_saved_W=False, num_epochs=1,
+                                                get_mapping_matrix=True, order='BALANCED_ORDER', W_init='RND_INT')
+        print matrix
+        writer = pd.ExcelWriter('rnd_init_uni_seq.xlsx')
         matrix.to_excel(writer, 'Sheet1')
         writer.save()
-        print matrix
-        #self.stam_obj.print_W()
+        self.stam_obj.print_W()
 
 
 
